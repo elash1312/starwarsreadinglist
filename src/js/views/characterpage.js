bfbf035597fb9d9b277211b450752ef31c58b795
elash1312 <lasherin1312@gmail.com>
2022-05-11 00:36:29 +0000

    more code

--- src/js/views/characterpage.js
+++ src/js/views/characterpage.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react-router-dom";
 
 export const CharacterPage = () => {
     const params = useParams();
+    const [character, setCharacter] = React.useState(null)
+
+    React.useEffect(() => {
+        const fn = asynch() => {
+            const response = await fetch(
+                "https://www.swapi.dev/api/people/1/" + params.id
+            );
+            const payload = response.json();
+            setCharacter(payload.result.properties);
+        };
+        fn();
+    }, []);
 
     return (
-    <div>
-        <h1>Character Page {params.id} </h1>
-    </div>
+        <div>
+            <h1>Character Page {params.id} </h1>
+        </div>
     );
 };
