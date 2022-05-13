ee3c00a39c9ab7cc8d186c8968da86746d1e38ee
elash1312 <lasherin1312@gmail.com>
2022-05-13 00:55:57 +0000

    some small changes

--- src/js/component/charactercard.js
+++ src/js/component/charactercard.js
@@ -4,17 +4,23 @@ import { Link } from "react-router-dom";
 
 export const CharacterCard = (props) => {
     return (
-        <div className="card" style={{width: "18rem"}}>
-            <img src="..." className="card-img-top" alt="..."/>
-                <div className="card-body">
-                    <h5 className="card-title">{props.obj.name}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <Link to={`/character/${props.id}`}><button className="btn btn-primary">Learn More</button></Link>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col">
+                    <div className="card" style={{ width: "18rem" }}>
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{props.obj.name}</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <Link to={`/character/${props.id}`}><button className="btn btn-primary">Learn More</button></Link>
+                        </div>
+                    </div>
                 </div>
+            </div>
         </div>
     )
 };
 CharacterCard.propTypes = {
     obj: PropTypes.object,
     id: PropTypes.number
-  };
+};
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1"> <img src="https://www.shutterstock.com/image-photo/kiev-ukraine-september-11-2018-this-1303187173" className="card-img-top" alt="..." /></span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
